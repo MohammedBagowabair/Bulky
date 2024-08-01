e996806103bb1740370da22c65c6181ef88bfd5b
MohammedBagowabair <159595929+MohammedBagowabair@users.noreply.github.com>
2024-08-01 15:37:50 +0800

    2 - Input Tag Helpers For Category

--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -8,7 +8,9 @@ namespace BulkyWeb.Models
         [Key]
         public int Id { get; set; }
         [Required]
+        [Display(Name ="Category Name")]
         public string Name { get; set; }
+        [Display(Name = "Display Order")]
         public int DisplayOrder { get; set; }
     }
 }
