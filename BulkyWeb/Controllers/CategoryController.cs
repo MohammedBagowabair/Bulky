b01d877850a2b4e581949010229b026dd78d6654
MohammedBagowabair <159595929+MohammedBagowabair@users.noreply.github.com>
2024-08-01 18:45:51 +0800

    2 - Get and Post Action for Delete Category

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -55,17 +55,41 @@ namespace BulkyWeb.Controllers
         [HttpPost]
         public IActionResult Edit(Category obj)
         {
-            //if (obj.Name == obj.DisplayOrder.ToString())
-            //{
-            //    ModelState.AddModelError("name", "Name Cannot Exactly Same The Display Order");
-            //}
             if (ModelState.IsValid)
             {
-                db.Categories.Add(obj);
+                db.Categories.Update(obj);
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
             return View(obj);
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? CategoryFromDb = db.Categories.Find(id);
+            if (CategoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(CategoryFromDb);
+        }
+        [HttpPost,ActionName("Delete")]
+        public IActionResult DeletePost(int?id)
+        {
+            Category? CategoryFromDb = db.Categories.Find(id);
+            if (CategoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            db.Categories.Remove(CategoryFromDb);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+          
         }
     }
 }
