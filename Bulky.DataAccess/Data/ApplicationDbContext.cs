0fc8b5974f4c8eeccbcf3b7c80188cfd74e580ed
MohammedBagowabair <159595929+MohammedBagowabair@users.noreply.github.com>
2024-08-11 14:58:19 +0800

    9 - Add Company Model and Create its Table in Database

--- Bulky.DataAccess/Data/ApplicationDbContext.cs
+++ Bulky.DataAccess/Data/ApplicationDbContext.cs
@@ -15,6 +15,8 @@ namespace Bulky.DataAccess.Data
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<Company> Companies { get; set; }
+
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -112,7 +114,6 @@ namespace Bulky.DataAccess.Data
                    ImageUrl = ""
                }
                );
-            //base.OnModelCreating(modelBuilder);
         }
     }
 }
