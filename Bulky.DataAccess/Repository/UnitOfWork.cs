e09f5adf205b4af23a7c02399137d4fc17d6597e
MohammedBagowabair <159595929+MohammedBagowabair@users.noreply.github.com>
2024-08-11 15:11:29 +0800

    9 - Add Company To Repository and Unit Of Work

--- Bulky.DataAccess/Repository/UnitOfWork.cs
+++ Bulky.DataAccess/Repository/UnitOfWork.cs
@@ -14,12 +14,15 @@ namespace Bulky.DataAccess.Repository
         public ICategoryRepository Category { get; private set; }
 
         public IProductRepository  Product { get; private set; }
+        public ICompanyRepository Company { get; private set; }
+
 
         public UnitOfWork(ApplicationDbContext db)
         {
             this._db = db;
             Category=new CategoryRepository(_db);
             Product=new ProductRepository(_db);
+            Company=new CompanyRepository(_db);
         }
 
         public void Save()
