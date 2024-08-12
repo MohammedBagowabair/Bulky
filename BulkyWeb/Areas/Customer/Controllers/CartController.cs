8a755ca42d062cb97262e333c4360f0abc078060
MohammedBagowabair <159595929+MohammedBagowabair@users.noreply.github.com>
2024-08-12 18:21:48 +0800

    10 - Order Summary UI

--- BulkyWeb/Areas/Customer/Controllers/CartController.cs
+++ BulkyWeb/Areas/Customer/Controllers/CartController.cs
@@ -71,6 +71,11 @@ namespace BulkyWeb.Areas.Customer.Controllers
             return RedirectToAction(nameof(Index));
         }
 
+        public IActionResult Summary()
+        {
+            return View();
+        }
+
         private double GetPriceBasedOnQuantity(ShoppingCart shoppingCart)
         {
             if (shoppingCart.Count <= 50)
