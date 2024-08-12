d89d286ff36411633c0399472492eceb543fdef9
MohammedBagowabair <159595929+MohammedBagowabair@users.noreply.github.com>
2024-08-12 15:38:35 +0800

    10 - A weird bug

--- BulkyWeb/Areas/Customer/Controllers/HomeController.cs
+++ BulkyWeb/Areas/Customer/Controllers/HomeController.cs
@@ -56,6 +56,7 @@ namespace BulkyWeb.Areas.Customer.Controllers
                 //add cart record
                 _unitOfWork.ShoppingCart.Add(shoppingCart);
             }
+            TempData["success"] = "Cart updated successfully";
             _unitOfWork.Save();
             return RedirectToAction(nameof(Index)); 
         }
