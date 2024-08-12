4fd2afc126da242a6b3ff26f827ae8c30f5c79ee
MohammedBagowabair <159595929+MohammedBagowabair@users.noreply.github.com>
2024-08-12 14:17:33 +0800

    10 - Add Shopping Cart to Repository & UnitOfWork

--- Bulky.DataAccess/Repository/UnitOfWork.cs
+++ Bulky.DataAccess/Repository/UnitOfWork.cs
@@ -16,6 +16,8 @@ namespace Bulky.DataAccess.Repository
         public IProductRepository  Product { get; private set; }
         public ICompanyRepository Company { get; private set; }
 
+        public IShoppingCartRepository ShoppingCart {  get; private set; }
+
 
         public UnitOfWork(ApplicationDbContext db)
         {
@@ -23,6 +25,7 @@ namespace Bulky.DataAccess.Repository
             Category=new CategoryRepository(_db);
             Product=new ProductRepository(_db);
             Company=new CompanyRepository(_db);
+            ShoppingCart=new ShoppingCartRepository(_db);
         }
 
         public void Save()
